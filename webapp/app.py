18f500b42dfef126466d003d371a02c7bbbf3314
Sean J. Taylor <sjt@fb.com>
2015-05-17 17:30:13 +0200

    More configurable web server with simple MTurk integration

--- webapp/app.py
+++ webapp/app.py
@@ -2,6 +2,7 @@
 import json
 from uuid import uuid4
 from itertools import islice, cycle
+from argparse import ArgumentParser, FileType
 
 from tornado.ioloop import IOLoop
 from tornado.web import Application, RequestHandler, StaticFileHandler
@@ -15,24 +16,25 @@ class CueExperiment(SimpleExperiment):
     Randomly reorder the stories and choose from available cues.
     """
     def setup(self):
-        self.set_log_file('logs/experiment.log')
+        self.set_log_file('webapp/logs/experiment.log')
 
-    def assign(self, params, userid, story_keys):
-        params.story_keys = Sample(choices=story_keys, unit=userid)
+    def assign(self, params, userid, story_keys, n):
+        params.story_keys = Sample(choices=story_keys, unit=userid, draws=n)
         balanced_sources = list(islice(cycle([
             'msnbc',
             'cnn',
             'foxnews',
-        ]), len(story_keys)))
-        params.sources = Sample(choices=balanced_sources, unit=userid)
+        ]), n))
+        params.sources = Sample(choices=balanced_sources, unit=userid, draws=n)
 
 class Likert(SimpleExperiment):
     """
     A randomization of the survey scale for robustness of the politics measure.
+    This illustrates how PlanOut can be used to randomize scales in surveys.
     """
 
     def setup(self):
-        self.set_log_file('logs/survey.log')
+        self.set_log_file('webapp/logs/survey.log')
 
     def assign(self, params, userid):
         params.reversed_scale = UniformChoice(choices=[0,1], unit=userid)
@@ -89,23 +91,36 @@ class PoliticalSurvey(BaseRequestHandler):
 
 class Thanks(BaseRequestHandler):
     def get(self):
-        self.render('templates/thanks.html')
+        if self.get_secure_cookie('survey_taken') == '1':
+            userid = self.get_secure_cookie('userid')
+            code = hash(userid) % 1000000
+        else:
+            code = None
+
+        self.render('templates/thanks.html',
+                    code=code)
 
 class SummariseContent(BaseRequestHandler):
-    with open('stories.json') as f:
-        stories = {int(k): v for k, v in json.load(f).iteritems()}
-    story_keys = sorted(stories.keys())
+    
+
+    def initialize(self, stories, k, n):
+        self.stories = stories
+        self.story_keys = sorted(stories.keys())
+        self.k = k
+        self.n = n
  
     def get(self):
         exp = CueExperiment(
             userid=self.current_user,
             story_keys=self.story_keys,
+            n=self.n,
         )
 
         stories = [(k, self.stories[k]) for k in exp.get('story_keys')]
         self.render(
             'templates/story.html', 
-            k=3,
+            k=self.k,
+            n=self.n,
             sources=exp.get('sources'),
             stories=stories,
         )
@@ -113,24 +128,41 @@ class SummariseContent(BaseRequestHandler):
     def post(self):
         exp = CueExperiment(
             userid=self.current_user,
-            story_keys=self.story_keys
+            story_keys=self.story_keys,
+            n=self.n,
         )
         exp.log_event('summary', self.request.arguments)
         self.redirect('/survey')
 
 
 if __name__ == '__main__':
-    app = Application(
-        [
-            (r'/', SummariseContent),
-            (r'/survey', PoliticalSurvey),
-            (r'/thanks', Thanks),
-            (r'/static/(.*)', StaticFileHandler, {"path": "static"})
-        ],
-        debug=False,
-        static_path='static',
-        cookie_secret='0.2752290303981113',
+    parser = ArgumentParser()
+    parser.add_argument('--n', default=10, type=int, 
+                        help='How many stories to show.')
+    parser.add_argument('--k', default=3, type=int,
+                        help='How many the subject should select.')
+    parser.add_argument('--stories', default='webapp/stories.json', 
+                        type=FileType('r'),
+                        help='Source of stories.')
+    parser.add_argument('--port', default=9000, type=int)
+    parser.add_argument('--host', default='127.0.0.1')
+    parser.add_argument('--debug', default=False, action='store_true')
+    args = parser.parse_args()
+
+    stories = {int(k): v for k, v in json.load(args.stories).iteritems()}
+
+    app = Application([
+        (r'/', SummariseContent, {'stories': stories, 
+                                  'n': args.n, 
+                                  'k': args.k}),
+        (r'/survey', PoliticalSurvey),
+        (r'/thanks', Thanks),
+        (r'/static/(.*)', StaticFileHandler, {'path': 'static'})
+    ],
+        debug=args.debug,
+        static_path='webapp/static',
+        cookie_secret='0.27522903039811131',
     )
     http_server = HTTPServer(app)
-    http_server.listen(9000, address='127.0.0.1')
+    http_server.listen(args.port, address=args.host)
     IOLoop.instance().start()
--- webapp/extract_data.py
+++ webapp/extract_data.py
@@ -2,37 +2,64 @@
 import sys
 import json
 import pandas as pd
+from argparse import ArgumentParser, FileType
 
-def main(fn, output):
-    exposures = []
-    outcomes = []
-    with open(fn) as f:
-        for line in f:
-            obj = json.loads(line)
-            userid = obj['inputs']['userid']
+def main(args):
+    results = []
+    for line in args.infile:
+        obs = json.loads(line)
 
-            if obj['event'] == 'exposure':
-                p = obj['params']
-                for position, (sk, source) in enumerate(zip(p['story_keys'], p['sources'])):
-                    exposures.append({
-                        'userid': userid,
-                        'position': position,
-                        'story_key': sk,
-                        'source': source,
-                        })
-            elif obj['event'] == 'summary':
-                for sk, summary in obj['extra_data'].iteritems():
-                    outcomes.append({
-                        'userid': userid,
-                        'story_key': int(sk),
-                        'summary': summary[0],
-                    })
+        # Make up an integer useid for convenience.
+        userid = hash(obs['inputs']['userid']) % 100000
+        time = obs['time']
 
-    e = pd.DataFrame(exposures).set_index(['userid', 'story_key'])
-    o = pd.DataFrame(outcomes).set_index(['userid', 'story_key'])
+        if obs['event'] == 'exposure' == args.event:
+            for exposure in extract_exposures(obs, userid, time):
+                results.append(exposure)
 
-    e.join(o).to_csv(output)
+        elif obs['event'] == 'summary' == args.event:
+            for outcome in extract_outcomes(obs, userid, time):
+                results.append(outcome)
 
+        elif obs['event'] == 'response' == args.event:
+            for response in extract_survey_response(obs, userid, time):
+                results.append(response)
+
+    pd.DataFrame(results).to_csv(args.outfile, index=False)
+
+def extract_exposures(obs, userid, time):
+    p = obs['params']
+    for position, (sk, source) in enumerate(zip(p['story_keys'], p['sources'])):
+        yield {
+            'userid': userid,
+            'time': time,
+            'position': position,
+            'story_key': sk,
+            'source': source,
+            }
+    
+def extract_outcomes(obs, userid, time):
+    for sk, summary in obs['extra_data'].iteritems():
+        yield {
+            'userid': userid,
+            'time': time,
+            'story_key': int(sk),
+            'summary': int(len(summary[0]) > 0),
+        }
+
+def extract_survey_response(obs, userid, time):
+    yield {
+        'userid': userid,
+        'time': time,
+        'response': obs['extra_data']['resp'],
+        'reversed_scale': obs['params']['reversed_scale'],
+    }
 
 if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2])
+    parser = ArgumentParser()
+    parser.add_argument('--event')
+    parser.add_argument('--infile', type=FileType('r'))
+    parser.add_argument('--outfile', type=FileType('w'))
+
+    args = parser.parse_args()
+    main(args)
